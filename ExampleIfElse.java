8b65473fb36a8a2a486cc6a75d062be3c4d2fd9b
sarthak1132003 <159808143+sarthak1132003@users.noreply.github.com>
2024-03-05 13:40:42 +0530

    Create ExampleIfElse.java

---
+++ ExampleIfElse.java
@@ -0,0 +1,17 @@
+
+/*Write a program to check the person is eligible for voting or not */
+class ExampleIfElse 
+{
+ public static void main(String[] args) 
+{
+int age=15;
+if(age>=18)
+{
+System.out.println("Person is eligible for voting");
+}
+else
+{
+System.out.println("Person is Not eligible for voting");
+}
+}
+}
