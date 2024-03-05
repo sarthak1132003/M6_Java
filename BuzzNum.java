6e9b537768619aed1ec78b7d448a8716f2baf640
sarthak1132003 <159808143+sarthak1132003@users.noreply.github.com>
2024-03-05 13:41:40 +0530

    Create BuzzNum.java

---
+++ BuzzNum.java
@@ -0,0 +1,23 @@
+/*WAP to check the num is buzz no or not */
+class BuzzNum
+{
+public static void main (String[]args)
+{
+int num=97diese;
+if (num<100 && num>=0)
+{
+if(num%7==0 || num%10==7)
+{
+System.out.println("Given Num is Buzz No");
+}
+else
+{
+System.out.println("Given Num is Not Buzz No");
+}
+}
+else
+{
+System.out.println("Given Num is more than 2 digit");
+}
+}
+}
